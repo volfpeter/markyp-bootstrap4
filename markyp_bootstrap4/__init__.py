2bad0c85acefa1b1e55b7c28db8a8d2c1e49b558
Péter Volf <peter@milinnovations.com>
2019-04-20 17:45:32 +0200

    added __all__ and bumped version to 0.1905.0

--- markyp_bootstrap4/__init__.py
+++ markyp_bootstrap4/__init__.py
@@ -12,7 +12,10 @@ __copyright__ = "Copyright 2019, Peter Volf"
 __email__ = "do.volfp@gmail.com"
 __license__ = "MIT"
 __url__ = "https://github.com/volfpeter/markyp-bootstrap4"
-__version__ = "0.1904.0"
+__version__ = "0.1905.0"
+
+
+__all__ = ("CDNs", "req")
 
 
 class CDNs(object):
