e686a8fb810ec31ed0d8e3d70af934eb41c96fa4
Péter Volf <peter@milinnovations.com>
2019-05-31 09:01:33 +0200

    nitpicking in scrollspy

--- markyp_bootstrap4/scrollspy.py
+++ markyp_bootstrap4/scrollspy.py
@@ -10,18 +10,19 @@ __all__ = ("spied_by",)
 from markyp import PropertyDict
 
 
-def spied_by(spy_id: str, offset: int = 0) -> PropertyDict:
+def spied_by(spy: str, *, offset: int = 0) -> PropertyDict:
     """
     Returns a `PropertyDict` whose items must be added to the spied element as attributes
     (it is usually done by `element(..., **spied_by())`).
 
     Arguments:
-        spy_id: The ID of the element that is spying the scroll position
-                (typically a `nav` or `list_group` with anchor elements in it).
+        spy: The ID of the element (typically a `nav` or `list_group` with anchor elements in it)
+             or the CSS class of the elements (in which case the attribute must start with a `.`
+             character) that are spying on the scroll position.
         offset: Pixels to offset from top when calculating position of scroll.
     """
     return {
         "data-spy": "scroll",
-        "data-target": f"#{spy_id}",
+        "data-target": spy if spy[0] in (".", "#") else f"#{spy}",
         "data-offset": offset
     }
